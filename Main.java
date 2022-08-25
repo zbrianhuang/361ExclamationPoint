901dbc67340d29f0c13cffd4edbd5be87926c773
zbrianhuang <67428806+zbrianhuang@users.noreply.github.com>
2022-08-25 00:18:22 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -23,9 +23,7 @@ public class Main {
 		int input=0;
 		
 		Scanner in = new Scanner(System.in);
-		for(int i: addBinary(convertToBinary(1),convertToBinary(9))) {
-			System.out.print(i);
-		}
+		
 		
 
 		while(!pass) {
@@ -41,9 +39,25 @@ public class Main {
 				pass=false;
 			}
 		}
+		for(int i: factorial(input)) {
+			System.out.print(i);
+		}
 		
 		
-		
+	}
+	public static ArrayList<Integer> factorial(int in){
+		ArrayList<ArrayList<Integer>> productList=new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> result=new ArrayList<Integer>();
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		temp.add(1);
+		productList.add(temp);
+		for(int i= 0;i<in;i++) {
+			productList.add(multiplyBinary(productList.get(i),convertToBinary(i+1)));
+		}
+		for(int j = 0;j<productList.size();j++) {
+			result=addBinary(result,productList.get(j));
+		}
+		return result;
 	}
 	public static ArrayList<Integer> convertToBinary(int in) {
 	//public static void convertToBinary(int in) {
@@ -62,14 +76,82 @@ public class Main {
 	}
 	public static ArrayList<Integer> multiplyBinary(ArrayList<Integer> a, ArrayList<Integer> b){ 
 		ArrayList<Integer> product= new ArrayList<Integer>();
-		ArrayList<Integer> subProducts= new ArrayList<Integer>();
+		//product.add(0);
+		ArrayList<ArrayList<Integer>> subProducts= new ArrayList<ArrayList<Integer>>();
 		String intAsChar;
 		char temp;
-		for(int i= 0;i<a.size();i++) {
-			intAsChar = a.get(i).toString();
-			temp = intAsChar.charAt(0);
+		int smallerArraySize = a.size();
+		int largerArraySize = b.size();
+		boolean aIsBigger=false;
+		int baseCount = 0;
+		ArrayList<Integer> singleProduct = new ArrayList<Integer>();
+		if(b.size()<a.size()) {
+			aIsBigger=true;
+			smallerArraySize=b.size();
+			largerArraySize=a.size();
+		}
+		
+	
+		for(int j = 0;j<smallerArraySize;j++) {
+			
+			
+			singleProduct=new ArrayList<Integer>();
+
+			for(int k= 0;k<baseCount;k++) {
+				System.out.println("added "+k+" zeros");
+				singleProduct.add(0,0);
+			}
+			System.out.println();
+			
+			for(int i= 0;i<largerArraySize;i++) {
+				
+				
+				
+				
+				if(aIsBigger) {
+					if(b.get(j)==1) {
+						singleProduct.add(a.get(i));
+					}else {
+						singleProduct.add(0);
+					}
+				}else {
+					if(a.get(j)==1) {
+						singleProduct.add(b.get(i));
+
+					}else {
+						singleProduct.add(0);
+					}
+					
+				}
+
+			}
+			System.out.println("base "+baseCount);
+			
+			subProducts.add(singleProduct);
+			baseCount++;
+			
+		//
+			for(ArrayList<Integer> it: subProducts) {
+				for( int sdf: it) {
+					System.out.print(+sdf);
+					
+				}
+				System.out.println("Subproduct");
+				
+			}
+		}
+		//
+		
+		
+		for(int i= 0;i<subProducts.size();i++) {
+			product=addBinary(product,subProducts.get(i));
+			
 			
 		}
+		
+			
+		
+		System.out.println();
 		return product;
 	}
 	//numbers should be inputted backwards
@@ -81,22 +163,21 @@ public class Main {
 		String intAsChar;
 		int regroup=0;
 		int digit;
+		int diff;
 		if(a.size()>b.size()) {
-			for(int i= 0;i<a.size()-b.size();i++) {
+			diff=a.size()-b.size();
+			for(int i= 0;i<diff;i++) {
+
 				b.add(0);
 			}
+			//b.add(0);//idk man it works.
 		}
 		if(b.size()>a.size()) {
-			for(int i= 0;i<b.size()-a.size();i++) {
+			diff=b.size()-a.size();
+			for(int i= 0;i<diff;i++) {
+
 				a.add(0);
 			}
-		}
-		
-		for(int r:a) {
-			System.out.println("A:"+r);
-		}
-		for(int q:b) {
-			System.out.println("B:"+q);
 		}
 		
 		for(int i= 0;i<a.size();i++) {
